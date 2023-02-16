d81fe451897adbd53f5874b47df2f3135ba5578b
Olga Suvorova <olga.suv.ya@yandex.ru>
2023-02-16 23:29:42 +0300

    encoding

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -10,8 +10,8 @@ import static com.codeborne.selenide.Selenide.$$;
 
 
 public class DashboardPage {
-    private final String balanceStart = "баланс: ";
-    private final String balanceFinish = " р.";
+    private final String balanceStart = "Р±Р°Р»Р°РЅСЃ: ";
+    private final String balanceFinish = " СЂ.";
     private SelenideElement heading = $("[data-test-id=dashboard]");
     private ElementsCollection cards = $$(".list__item div");
 
